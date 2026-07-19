a0531e08c6599597d3f425ab364cfdbfa00b5a93
xpburn-hue <xpburn@gmail.com>
2026-07-19 08:23:37 -0400

    Add initial measurement models

---
+++ src/models/mod.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Clone, Copy)]
+pub struct DistanceYards(pub f64);
+
+#[derive(Debug, Clone, Copy)]
+pub struct VelocityFps(pub f64);
+
+#[derive(Debug, Clone, Copy)]
+pub struct BulletWeightGrains(pub f64);
+
+#[derive(Debug, Clone, Copy)]
+pub struct BallisticCoefficient(pub f64);
