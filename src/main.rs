5b2571faed84de79a29e187319631e0d600ded2d
xpburn-hue <xpburn@gmail.com>
2026-07-19 11:01:32 -0400

    Add basic CLI API entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bc_assist::api::{calculate_trajectory, request::TrajectoryRequest};
+
 fn main() {
-    println!("bc-assist");
+    let request = TrajectoryRequest {
+        distance_yards: 100.0,
+    };
+
+    let response = calculate_trajectory(request);
+    println!("{} yards: {}", response.requested_distance_yards, response.status);
 }
