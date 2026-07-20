a156316b1c259c4416f981f2fbd3a630e0a6171e
xpburn-hue <xpburn@gmail.com>
2026-07-20 16:14:14 -0400

    Wire corrected drag into physics model

--- src/ballistic/physics.rs
+++ src/ballistic/physics.rs
@@ -1,3 +1,7 @@
+use super::atmosphere::Atmosphere;
+use super::bc::BallisticCoefficient;
+use super::drag::bc::retardation_with_bc;
+use super::drag::table::DragTableEntry;
 use super::state::StateVector;
 
 pub trait DragModel {
@@ -10,6 +14,29 @@ pub struct NoDrag;
 impl DragModel for NoDrag {
     fn acceleration(&self, _state: &StateVector) -> (f64, f64) {
         (0.0, 0.0)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct BallisticDrag {
+    pub table: &'static [DragTableEntry],
+    pub bc: BallisticCoefficient,
+    pub atmosphere: Atmosphere,
+}
+
+impl DragModel for BallisticDrag {
+    fn acceleration(&self, state: &StateVector) -> (f64, f64) {
+        let speed = (state.velocity_x.powi(2) + state.velocity_y.powi(2)).sqrt();
+        let drag = retardation_with_bc(self.table, speed, self.bc, self.atmosphere);
+
+        if speed <= 0.0 {
+            return (0.0, 0.0);
+        }
+
+        (
+            -drag * state.velocity_x / speed,
+            -drag * state.velocity_y / speed,
+        )
     }
 }
 
